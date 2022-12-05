b6a3bb42a5380662bf1c8d01c8f233dc27c9da55
Zehra Şeren <94180168+zehraseren@users.noreply.github.com>
2022-12-05 05:52:50 +0300

    Update if-else-hackerrank-solution.js

--- if-else-hackerrank-solution.js
+++ if-else-hackerrank-solution.js
@@ -25,16 +25,16 @@ function readLine() {
 function main() {
     const N = parseInt(readLine().trim(), 10); // Olduğu gibi bırakıyoruz.
     
-    if (N%2 == 1) { //Burada ```% (mod alma)``` operatörünü kullanarak ```N```'in tek mi çift mi olduğunu gösteriyoruz.
+    if (N%2 == 1) { //Burada "% (mod alma)" operatörünü kullanarak "N"in tek mi çift mi olduğunu gösteriyoruz.
         console.log ("Weird");
     }
-    else if ((N%2 == 0) && (2<=N) && (N<=5)) { //Burada ```N```'in 2 ile 5 arasında olup olmadığına bakıyoruz.
+    else if ((N%2 == 0) && (2<=N) && (N<=5)) { //Burada "N"in 2 ile 5 arasında olup olmadığına bakıyoruz.
         console.log ("Not Weird");
     }
-    else if ((N%2 == 0) && (N>=6) && (N<=20)) { //Burada ```N```'in 6 ile 20 arasında olup olmadığına bakıyoruz.
+    else if ((N%2 == 0) && (N>=6) && (N<=20)) { //Burada "N"'in 6 ile 20 arasında olup olmadığına bakıyoruz.
         console.log ("Weird");
     }
-    else if ((N%2 == 0) && (N>20)) { //Burada ```N```'in 20'den büyük olmadığına bakıyoruz.
+    else if ((N%2 == 0) && (N>20)) { //Burada "N"'in 20'den büyük olmadığına bakıyoruz.
         console.log ("Not Weird");
     }
 }
