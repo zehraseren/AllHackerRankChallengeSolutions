15d8f8cb7fc30b39923774e81ec809b8dba251f3
Zehra Şeren <94180168+zehraseren@users.noreply.github.com>
2022-12-07 08:37:36 +0300

    Update functions-factorial.js

--- functions-factorial.js
+++ functions-factorial.js
@@ -1,1 +1,34 @@
+'use strict';
 
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// *************************************KODU YAZMANIZ GEREKEN KISIM BURASI*************************************
+function factorial (n) { //Recursion kısmındaki verieln örneği referans alarak yapabilirsiniz.
+    return (n === 1) ? n : (n*factorial(n-1))
+}
+
+function main() {
+    const n = +(readLine());
+    
+    console.log(factorial(n));
+}
